07258896305cbb0f80d025ec025fbd2bc375859f
Jimmy31071987 <Jimmy31071987@gmail.com>
2013-08-06 19:19:10 -0400

    Update ajax.js
    One line condition to reduce size of file.

--- ajax.js
+++ ajax.js
@@ -1,22 +1,14 @@
 function request(method, url, data, callback) {
-	if (window.XMLHttpRequest) {
-		var req = new XMLHttpRequest();
-	} else {
-		// Internet Explorer
-		var req = new ActiveXObject('Microsoft.XMLHTTP');
-	}
-
+	
+	
+	var req=window.XMLHttpRequest?new XMLHttpRequest():new ActiveXObject('Microsoft.XMLHTTP');
 	if (method === 'GET' && typeof data === 'string') {
 		url += '?' + data;
 	}
-
 	req.open(method, url, true);
-
 	if (method === 'POST' && typeof data === 'string') {
-		req.setRequestHeader('Content-type',
-			'application/x-www-form-urlencoded');
+		req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
 	}
-
 	req.onreadystatechange = function () {
 		if (req.readyState === 4 && req.status === 200) {
 			var contentType = req.getResponseHeader('Content-type');
